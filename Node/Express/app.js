4404718c57b6fc83b59a6bfae865a8467a193d05
LoganHodgins <loganhodginsofficial@gmail.com>
2023-04-04 01:51:00 -0500

    tut files

--- Node/Express/app.js
+++ Node/Express/app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const path = require('path');
 
 const bodyParser = require('body-parser');
 const express = require('express');
@@ -9,8 +10,13 @@ const shopRoutes = require('./routes/shop');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(3000);
